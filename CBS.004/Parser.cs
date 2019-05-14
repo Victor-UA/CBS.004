7036b7aed4fee2792ab5774aac18d32cb58e32f0
Victor <Victor_UA@Ukr.net>
2019-05-14 11:24:52 +0300

    Done

---
+++ CBS.004/Parser.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CBS._004
+{
+    class Parser
+    {
+        public Parser(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get; }
+
+
+        public ICollection Parse(string pattern)
+        {
+            var regex = new Regex(pattern);
+            var result = regex.Matches(Text); 
+            return result;
+        }
+
+        public ICollection Parse2File(string caption, string pattern, string path, ICollection<string> regexGroups = null)
+        {
+            var result = Parse(pattern);
+            SaveCollectionToFile(caption, result as MatchCollection, path, regexGroups);
+            return result;
+        }
+
+        private void SaveCollectionToFile(string caption, MatchCollection collection, string path, ICollection<string> regexGroups = null)
+        {
+            using (var streamWriter = File.AppendText(path))
+            {
+                try
+                {
+                    streamWriter.WriteLine(caption);
+                    foreach (Match item in collection)
+                    {
+                        StringBuilder builder = new StringBuilder();
+                        if (regexGroups != null)
+                        {
+                            foreach (var @group in regexGroups)
+                            {
+                                builder.Append($"{item.Groups[@group]} : ");
+                            }
+
+                            builder.Remove(builder.Length - 3, 3);
+                            streamWriter.WriteLine($"{builder}");
+                        }
+                        else
+                        {
+                            streamWriter.WriteLine(item.Value);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    streamWriter.Close();
+                }
+            }            
+        }
+    }
+}
--- CBS.004/Program.cs
+++ CBS.004/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +12,55 @@ namespace CBS._004
 {
     class Program
     {
+        // Напишите программу, которая бы позволила вам по указанному адресу web-страницы
+        // выбирать все
+
+        // ссылки на другие страницы,
+        // номера телефонов,
+        // почтовые адреса
+
+        // и сохраняла полученный результат в файл.
+
+
+        private const string FILE_PATH = "ParsingResult.txt";
+        private const string HREF_PATTERN = "<a[^>]* href=\"(?<link>(http([^ \"]*)))\"";                                             
+        private const string PHONENUMBER_PATTERN = @"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}";
+        private const string EMAIL_PATTERN = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
+
         static void Main(string[] args)
+        {            
+            string pageContent = DownloadPage("https://ru.wikipedia.org/wiki/%D0%93%D1%80%D0%B0%D0%BD%D0%B6");
+            var parser = new Parser(pageContent);
+            if (File.Exists(FILE_PATH))
+            {
+                try
+                {
+                    File.Delete(FILE_PATH);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+            var hRefCollection = parser.Parse2File("References:", HREF_PATTERN, FILE_PATH, new List<string>{"link"});
+            var phoneNumberCollection = parser.Parse2File("Phone numbers:", PHONENUMBER_PATTERN, FILE_PATH);
+            var emailCollection = parser.Parse2File("E-mails:", EMAIL_PATTERN, FILE_PATH);
+
+            //Console.Write(pageContent);
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        private static string DownloadPage(string url)
         {
+            var client = new HttpClient();
+            var response = client.GetAsync(url).Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ArgumentException("Cannot download page via this url");
+            }
+            return response.Content.ReadAsStringAsync().Result;
         }
     }
 }
